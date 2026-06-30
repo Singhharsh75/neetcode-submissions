82344fd0606c25ac98e5d671257f33e261f67625
Harsh Singh <84003440+Singhharsh75@users.noreply.github.com>
2026-06-30 18:02:33 +0530

    Add: linked-list-cycle-detection - submission-6

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-6.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let map= new Map();
+        let tort=head; let hare=head;
+
+        while(tort && tort.next && hare && hare.next &&hare.next.next){
+            tort=tort.next;
+            hare=hare.next.next;
+            if(tort===hare)return true;
+        }
+
+        return false;
+    }
+}
