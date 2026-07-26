879b3da2093c06bd59de530b0d14a93415ad2905
Harsh Singh <84003440+Singhharsh75@users.noreply.github.com>
2026-07-26 11:33:27 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {number[]}
+     */
+    findOrder(numCourses, prerequisites) {
+        let indegree=Array(numCourses).fill(0)
+        let adj=Array.from({length:numCourses},()=>[])
+
+        for(let [des,src] of prerequisites){
+            adj[src].push(des)
+            indegree[des]++
+        }
+
+        let q=[]
+        for(let i=0;i<numCourses;i++){
+            if(indegree[i]===0)q.push(i)
+        }
+
+        let result=[]
+        while(q.length){
+            const node=q.shift()
+            result.push(node)
+            for(let nei of adj[node]){
+                indegree[nei]--;
+                if(indegree[nei]===0){
+                    q.push(nei)
+                }
+            }
+        }
+
+        return result.length===numCourses?result:[]
+    }
+}
